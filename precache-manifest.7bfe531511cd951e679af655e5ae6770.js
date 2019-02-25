81ace259c84d8a9a0819c305828bc229a3c7742b
Wade <liuweixi0819@gmail.com>
2019-02-25 09:48:58 -0500

    upload

---
+++ precache-manifest.7bfe531511cd951e679af655e5ae6770.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d5ca073a7de4b459c4d",
+    "url": "/static/css/main.6b33f013.chunk.css"
+  },
+  {
+    "revision": "9d5ca073a7de4b459c4d",
+    "url": "/static/js/main.9d5ca073.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "170d78dc7eaf86966fd3",
+    "url": "/static/js/2.170d78dc.chunk.js"
+  },
+  {
+    "revision": "541e8ae511f3048ea79db4562812fcef",
+    "url": "/index.html"
+  }
+];
